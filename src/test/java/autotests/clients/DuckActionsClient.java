32b1419f4c9f587da37463795c0c140d6cacfa04
NikSasha1234 <aleksandra_moxova@mail.ru>
2023-07-17 13:11:38 +0700

    Created HttpClient and added all Duck's methods.

---
+++ src/test/java/autotests/clients/DuckActionsClient.java
@@ -0,0 +1,109 @@
+package autotests.clients;
+
+import autotests.EndpointConfig;
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.http.client.HttpClient;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+
+@ContextConfiguration(classes = {EndpointConfig.class})
+
+public class DuckActionsClient extends TestNGCitrusSpringSupport {
+
+    @Autowired
+    protected HttpClient yellowDuckService;
+
+    @Description("Эндпоинт, создающий уточку")
+    public void createDuck(TestCaseRunner runner, String color, String height, String material, String sound, String wingsState) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .post("/api/duck/create")
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body("{\n" +
+                        "  \"color\": " + "\"" + color + "\",\n" +
+                        "  \"height\": " + height + ",\n" +
+                        "  \"material\": " + "\"" + material + "\",\n" +
+                        "  \"sound\": " + "\"" + sound + "\",\n" +
+                        "  \"wingsState\": " + "\"" + wingsState + "\"\n" +
+                        "}"));
+    }
+
+    @Description("Эндпоинт, удаляющий уточку")
+    public void deleteDuck(TestCaseRunner runner, String id) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .delete("/api/duck/delete")
+                .queryParam("id", id));
+    }
+
+    @Description("Эндпоинт для получения списка id всех уточек")
+    public void listDuckId(TestCaseRunner runner) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .get("/api/duck/getAllIds"));
+    }
+
+    @Description("Эндпоинт для обновления характеристик уточки")
+    public void updateDuck(TestCaseRunner runner, String color, String height, String id, String material, String sound, String wingsState) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .put("/api/duck/update")
+                .queryParam("color", color)
+                .queryParam("height", height)
+                .queryParam("id", id)
+                .queryParam("material", material)
+                .queryParam("sound", sound)
+                .queryParam("wingsState", wingsState)
+        );
+    }
+    @Description("Эндпоинт, заставляющий уточку лететь")
+    public void duckFly(TestCaseRunner runner, String id) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .get("/api/duck/action/fly")
+                .queryParam("id", id));
+    }
+
+    @Description("Эндпоинт, показывающий характеристики уточки")
+    public void duckProperties(TestCaseRunner runner, String id) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .get("/api/duck/action/properties")
+                .queryParam("id", id));
+    }
+
+    @Description("Эндпоинт, заставляющий уточку крякать")
+    public void duckQuack(TestCaseRunner runner, String id, String repetitionCount, String soundCount) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .get("/api/duck/action/quack")
+                .queryParam("id", id)
+                .queryParam("repetitionCount", repetitionCount)
+                .queryParam("soundCount", soundCount));
+    }
+
+
+    @Description("Эндпоинт, заставляющий уточку плыть")
+    public void duckSwim(TestCaseRunner runner, String id) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .get("/api/duck/action/swim")
+                .queryParam("id", id));
+    }
+}
+
+
